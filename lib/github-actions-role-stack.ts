ee4a986971cb0777b9b3536047f1f7d472cb3392
Concept Coding Geeks <conceptcodinggeeks@gmail.com>
2025-04-12 12:58:35 +0530

    feature/add-iam-role

--- lib/github-actions-role-stack.ts
+++ lib/github-actions-role-stack.ts
@@ -1,16 +1,36 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from "constructs";
+import { Stack, StackProps, aws_iam as iam } from "aws-cdk-lib";
 
-export class GithubActionsRoleStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class GithubActionsRoleStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const identityProvider = new iam.OpenIdConnectProvider(
+      this,
+      "IdentityProvider",
+      {
+        url: "https://token.actions.githubusercontent.com",
+        clientIds: ["sts.amazonaws.com"],
+        thumbprints: ["6938fd4d98bab03faadb97b34396831e3780aea1"], // Adding Thumbprints: https://github.blog/changelog/2022-01-13-github-actions-update-on-oidc-based-deployments-to-aws/
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GithubActionsRoleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // IAM Role assumed by Github Actions
+    const githubRole = new iam.Role(this, "GithubActionsRole", {
+      roleName: "github-actions-iam-role",
+      description: "IAM Role for Github Actions to assume",
+      assumedBy: new iam.OpenIdConnectPrincipal(
+        identityProvider
+      ).withConditions({
+        StringLike: {
+          "token.actions.githubusercontent.com:sub": [
+            "repo:concept-coding-geeks/*:*",
+          ],
+        },
+        StringEquals: {
+          "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+        },
+      }),
+    });
   }
 }
